04fcc73d3bf65d15f0f23d39b1a76e86cd4359f6
Siddharth  B N <143517174+Siddhubn@users.noreply.github.com>
2025-03-16 22:10:00 +0530

    Update apis.py

--- apis.py
+++ apis.py
@@ -1,15 +1,17 @@
 from pycoingecko import CoinGeckoAPI
 cg=CoinGeckoAPI()
+
 bitcoin_data=cg.get_coin_market_chart_by_id(id="bitcoin",vs_currency='usd',days=30)
+
 import pandas as pd
 data=pd.DataFrame(bitcoin_data['prices'],columns=['Timestamp','Price'])
 data['Date']=pd.to_datetime(data['Timestamp'],unit='ms')
 print(data['Date'])
 candlestick_data=data.groupby(data.Date.dt.data).agg({'Price':['min','max','first','last']})
 print(candlestick_data) 
+
 from nba_api.stats.static import teams
 import matplotlib.pyplot as plt
-
 def json_to_dict(json):
     ids=json[0].keys()
     dict={id:[] for id in ids}
@@ -17,9 +19,8 @@ def json_to_dict(json):
         for key,value in objects.items():
             dict[key].append(value)
     return dict
-
 nba_teams=teams.get_teams()
-import pandas as pd
+
 dict_nba_teams=json_to_dict(nba_teams)
 df_teams=pd.DataFrame(dict_nba_teams)
 print(df_teams.head())
